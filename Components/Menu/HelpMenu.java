be9f271527f7ad8594c4cb07fe215e1d29263ef0
SankalpKadam <sankalptp07@gmail.com>
2024-01-10 05:52:34 -0600

    Update HelpMenu.java

--- Components/Menu/HelpMenu.java
+++ Components/Menu/HelpMenu.java
@@ -1,14 +1,19 @@
 package Components.Menu;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
 
@@ -34,7 +39,7 @@ public class HelpMenu {
         Action helpaction = new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("about");
+                popUpBox();
             }
         };
         helpaction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_H, KeyEvent.CTRL_DOWN_MASK));
@@ -44,5 +49,24 @@ public class HelpMenu {
         helpMenu.add(helpItem);
         return helpMenu;
 
+    }
+    protected void popUpBox() {
+        JFrame popUpFrame = new JFrame();
+        popUpFrame.getContentPane().setBackground(Color.WHITE);
+        popUpFrame.setSize(300, 250);
+        popUpFrame.setResizable(true);
+
+        JTextArea np = new JTextArea();
+        np.setFont(new Font("sans-serif", Font.PLAIN, 16));
+        np.setBounds(0, 0, 300,250);
+        np.setEditable(false);
+        np.setText("Hi, my name is Sankalp Kadam and you are using the simple notepad that I have created."+"\n"+" I am currently pursuing Master's in Computer Science at The University of Texas at Arlington."+"\n"+" You can reach out to me on https://www.linkedin.com/in/sankalp-kadam."+"\n\n"+"@SankalpKadam");
+        np.setLineWrap(true);
+        np.setAlignmentY(250);
+
+        popUpFrame.setLayout(new BorderLayout());
+        popUpFrame.add(np, BorderLayout.CENTER);
+        popUpFrame.setAlwaysOnTop(true);
+        popUpFrame.setVisible(true);
     }
 }
