dabbe4cef9474ea4ac418b87e83b3744b23e49e7
SankalpKadam <sankalptp07@gmail.com>
2024-01-09 18:17:29 -0600

    Set all the keyboard shortcuts and action to be performed for the keystrokes.

--- NotepadGUI.java
+++ NotepadGUI.java
@@ -1,17 +1,19 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.AbstractButton;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
+
+import javax.swing.KeyStroke;
 
 public class NotepadGUI extends JFrame {
     Dimension screensizeDimension = Toolkit.getDefaultToolkit().getScreenSize();
@@ -38,15 +40,6 @@ public class NotepadGUI extends JFrame {
 
     }
 
-    private static void removeButton(Container optionsBox) {
-        Component[] c = optionsBox.getComponents();
-        for (Component component : c) {
-            if (component instanceof AbstractButton) {
-                optionsBox.remove(component);
-            }
-        }
-    }
-
     private static JPanel getMenuPanel() {
         // Creating the menu panel
         JPanel menuJPanel = new JPanel();
@@ -62,18 +55,23 @@ public class NotepadGUI extends JFrame {
         JMenuBar filMenuBar = new JMenuBar();
         filMenuBar.add(getFileOptionsMenu());
         filMenuBar.setBackground(Color.WHITE);
+        filMenuBar.setBorder(null);
 
         JMenuBar editMenuBar = new JMenuBar();
         editMenuBar.add(getEditOptionsMenu());
         editMenuBar.setBackground(Color.WHITE);
-        
+        editMenuBar.setBorder(null);
+
         JMenuBar helpMenuBar = new JMenuBar();
         helpMenuBar.add(getHelpOptionsMenu());
         helpMenuBar.setBackground(Color.WHITE);
+        helpMenuBar.setBorder(null);
+
         // Adding components to menu panel
         menuJPanel.add(filMenuBar);
         menuJPanel.add(editMenuBar);
         menuJPanel.add(helpMenuBar);
+
         return menuJPanel;
     }
 
@@ -89,13 +87,43 @@ public class NotepadGUI extends JFrame {
     private static JMenu getFileOptionsMenu() {
         // Creating the file Menu
         JMenu fileMenu = new JMenu("File");
-        //Creating menu items
+
+        // Creating menu items
         JMenuItem createFile = new JMenuItem("New");
+        // Creatin menu item action
+        Action createAction = new AbstractAction("New") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("New");
+            }
+        };
+        // setting keyboard shortcut to perform necessary action
+        createAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
+        createFile.setAction(createAction);
+
+        JMenuItem openFile = new JMenuItem("Open");
+        Action openAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Open");
+            }
+        };
+        openAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
+        openFile.setAction(openAction);
+        openFile.setText("Open");
+        JMenuItem saveFile = new JMenuItem("Save");
+        Action saveAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Save");
+            }
+        };
+        saveAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
+        saveFile.setAction(saveAction);
+        saveFile.setText("Save");
         fileMenu.add(createFile);
-        JMenuItem createFile1 = new JMenuItem("Open");
-        fileMenu.add(createFile1);
-        JMenuItem createFile2 = new JMenuItem("Save");
-        fileMenu.add(createFile2);
+        fileMenu.add(openFile);
+        fileMenu.add(saveFile);
 
         return fileMenu;
 
@@ -104,10 +132,47 @@ public class NotepadGUI extends JFrame {
     private static JMenu getEditOptionsMenu() {
         // Creating the edit Menu
         JMenu editMenu = new JMenu("Edit");
+        editMenu.setBackground(Color.WHITE);
 
-        editMenu.add(new JMenuItem("Copy"));
-        editMenu.add(new JMenuItem("Paste"));
-        editMenu.add(new JMenuItem("Cut"));
+        // Creating menu items
+        JMenuItem copyItem = new JMenuItem();
+        // Creating action for the menu item
+        Action copyAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("copy");
+            }
+        };
+        // Setting keyboard shortcut for the menu item
+        copyAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK));
+        copyItem.setAction(copyAction);
+        copyItem.setText("Copy");
+
+        JMenuItem pasteItem = new JMenuItem();
+        Action pasteAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("paste");
+            }
+        };
+        pasteAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_DOWN_MASK));
+        pasteItem.setAction(pasteAction);
+        pasteItem.setText("Paste");
+
+        JMenuItem cutItem = new JMenuItem();
+        Action cutAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("cut");
+            }
+        };
+        cutAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.CTRL_DOWN_MASK));
+        cutItem.setAction(cutAction);
+        cutItem.setText("Cut");
+
+        editMenu.add(copyItem);
+        editMenu.add(pasteItem);
+        editMenu.add(cutItem);
 
         return editMenu;
 
@@ -116,9 +181,19 @@ public class NotepadGUI extends JFrame {
     private static JMenu getHelpOptionsMenu() {
         // Creating the edit Menu
         JMenu helpMenu = new JMenu("Help");
+        helpMenu.setBackground(Color.WHITE);
+        JMenuItem helpItem = new JMenuItem();
+        Action helpaction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                System.out.println("about");
+            }
+        };
+        helpaction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_H, KeyEvent.CTRL_DOWN_MASK));
+        helpItem.setAction(helpaction);
+        helpItem.setText("About");
 
-        helpMenu.add(new JMenuItem("About"));
-
+        helpMenu.add(helpItem);
         return helpMenu;
 
     }
